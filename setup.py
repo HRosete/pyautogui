9dc5fdca0b6347ed255844cf0ebcd4ead40ab4cc
Hannah Rosete <Hannah.Rosete@analog.com>
2021-09-14 10:46:26 +0800

    Add setup.py
    Signed-off-by: Hannah Rosete <Hannah.Rosete@analog.com>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="",
+    version="",
+    description="",
+    url="",
+    author="Hannah Mae Nicole Rosete",
+    author_email="Hannah.Rosete@analog.com",
+    license="",
+    packages=[""],
+)
