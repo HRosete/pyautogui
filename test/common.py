c6c946b068b8b9828e7708364c2e2e6cf0069329
Hannah Rosete <Hannah.Rosete@analog.com>
2021-09-21 19:41:19 +0800

    Added an arg in the the locate_and_click fxn
    Signed-off-by: Hannah Rosete <Hannah.Rosete@analog.com>

--- test/common.py
+++ test/common.py
@@ -1,13 +1,17 @@
+import os
 import pyautogui
 
 
-def locate_and_click(img):
+def locate_and_click(dir, filename):
     """locate_and_click: Locate the specified image in the UI and click it.
 
-    parameter:
-        img: type=string
+    parameters:
+        dir: type=path
+            Absolute/relative path to the images folder
+        filename: type=string
             Filename with extension of the image you want to find
     """
-    image = pyautogui.locateCenterOnScreen(img, confidence=0.8)
+    location = os.path.join(dir, filename)
+    image = pyautogui.locateCenterOnScreen(location, confidence=0.8)
     pyautogui.click(image)
-    print(f"{img} clicked!")
+    print(f"{filename} clicked!")
