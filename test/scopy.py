1819cc27af87bc7ac2f714ef56b9a0f2beb2aa4c
Hannah Rosete <Hannah.Rosete@analog.com>
2021-09-21 19:43:24 +0800

    Edited locate_and_click args
    Signed-off-by: Hannah Rosete <Hannah.Rosete@analog.com>

--- test/scopy.py
+++ test/scopy.py
@@ -6,44 +6,9 @@ import time
 
 image_path = os.path.join(os.getcwd(), "scopy_images")
 popup_update_img = os.path.join(image_path, "auto_update_popup.png")
-yes_img = os.path.join(image_path, "yes.png")
-no_img = os.path.join(image_path, "no.png")
-close_img = os.path.join(image_path, "close.png")
-home_img = os.path.join(image_path, "home.png")
-m2k_img = os.path.join(image_path, "m2k.png")
-add_m2k = os.path.join(image_path, "add_m2k.png")
-cal_img = os.path.join(image_path, "calibrate.png")
-calibrating_img = os.path.join(image_path, "calibrating.png")
-connect_img = os.path.join(image_path, "connect.png")
-connecting_img = os.path.join(image_path, "connecting.png")
-disconnect_img = os.path.join(image_path, "disconnect.png")
-disconnect_2_img = os.path.join(image_path, "disconnect_2.png")
-add_img = os.path.join(image_path, "add.png")
-enable_demo_img = os.path.join(image_path, "enable_demo.png")
-disable_demo_img = os.path.join(image_path, "disable_demo.png")
-identify_img = os.path.join(image_path, "identify.png")
-
-osc_img = os.path.join(image_path, "oscilloscope.png")
-spectrum_analyzer_img = os.path.join(image_path, "spectrum_analyzer.png")
-network_analyzer_img = os.path.join(image_path, "network_analyzer.png")
-sig_gen_img = os.path.join(image_path, "signal_generator.png")
-logic_analyzer_img = os.path.join(image_path, "logic_analyzer.png")
-pattern_generator_img = os.path.join(image_path, "pattern_generator.png")
-dio_img = os.path.join(image_path, "digital_io.png")
-voltmeter_img = os.path.join(image_path, "voltmeter.png")
-power_img = os.path.join(image_path, "power_supply.png")
-
-
-# def locate_and_click(img):
-#     """locate_and_click: Locate the specified image in the UI and click it.
-
-#     parameter:
-#         img: type=string
-#             Filename with extension of the image you want to find
-#     """
-#     image = pyautogui.locateCenterOnScreen(img, confidence=0.8)
-#     pyautogui.click(image)
-#     print(f"{img} clicked!")
+# calibrating_img = os.path.join(image_path, "calibrating.png")
+# connecting_img = os.path.join(image_path, "connecting.png")
+# disconnect_2_img = os.path.join(image_path, "disconnect_2.png")
 
 
 def main():
@@ -64,17 +29,17 @@ def main():
     # Automatic Update Dialog Box
     popup = pyautogui.locateCenterOnScreen(popup_update_img, confidence=0.8)
     if popup:
-        locate_and_click(popup_update_img)
+        locate_and_click(image_path, "auto_update_popup.png")
         time.sleep(2)
-        # locate_and_click(yes_img)
-        # locate_and_click(no_img)
-        locate_and_click(close_img)
+        # locate_and_click(image_path, "yes.png")
+        # locate_and_click(image_path, "no.png")
+        locate_and_click(image_path, "close.png")
         time.sleep(2)
 
     # Connect to M2K
-    locate_and_click(m2k_img)
+    locate_and_click(image_path, "m2k.png")
     time.sleep(5)
-    locate_and_click(connect_img)
+    locate_and_click(image_path, "connect.png")
     time.sleep(2)
     pyautogui.moveRel(0, 100, duration=0.5)
 
@@ -91,59 +56,107 @@ def main():
     time.sleep(20)
 
     # Oscilloscope
-    locate_and_click(osc_img)
+    locate_and_click(image_path, "oscilloscope.png")
     time.sleep(5)
     import scopy_instruments.oscilloscope as osc
     osc.main()
 
+    # Spectrum Analyzer
+    locate_and_click(image_path, "spectrum_analyzer.png")
+    time.sleep(5)
+    import scopy_instruments.spectrum_analyzer as spec
+    spec.main()
+
+    # Network Analyzer
+    locate_and_click(image_path, "network_analyzer.png")
+    time.sleep(5)
+    import scopy_instruments.network_analyzer as network
+    network.main()
+
+    # Signal Generator
+    locate_and_click(image_path, "signal_generator.png")
+    time.sleep(5)
+    import scopy_instruments.signal_generator as sig_gen
+    sig_gen.main()
+
+    # Logic Analyzer
+    locate_and_click(image_path, "logic_analyzer.png")
+    time.sleep(5)
+    import scopy_instruments.logic_analyzer as logic
+    logic.main()
+
+    # Pattern Generator
+    locate_and_click(image_path, "pattern_generator.png")
+    time.sleep(5)
+    import scopy_instruments.pattern_generator as pattern
+    pattern.main()
+
+    # Digital IO
+    locate_and_click(image_path, "digital_io.png")
+    time.sleep(5)
+    import scopy_instruments.digital_io as dio
+    dio.main()
+
+    # Voltmeter
+    locate_and_click(image_path, "voltmeter.png")
+    time.sleep(5)
+    import scopy_instruments.voltmeter as volt
+    volt.main()
+
+    # Power Supply
+    locate_and_click(image_path, "power_supply.png")
+    time.sleep(5)
+    import scopy_instruments.power_supply as power
+    power.main()
+
     # Add M2K through IP
-    locate_and_click(home_img)
+    locate_and_click(image_path, "home.png")
     time.sleep(5)
     ip = "192.168.2.1"
-    locate_and_click(add_m2k)
+    locate_and_click(image_path, "add_m2k.png")
     time.sleep(2)
     pyautogui.moveRel(0, 155, duration=0.5)
     pyautogui.click()
     pyautogui.PAUSE
     pyautogui.write(ip, interval=0.1)
     pyautogui.PAUSE
-    locate_and_click(connect_img)
+    locate_and_click(image_path, "connect.png")
     time.sleep(2)
-    locate_and_click(add_img)
+    locate_and_click(image_path, "add.png")
     time.sleep(2)
-    locate_and_click(connect_img)
+    locate_and_click(image_path, "connect.png")
     time.sleep(2)
 
     # Enable Demo Mode
-    locate_and_click(home_img)
+    locate_and_click(image_path, "home.png")
     time.sleep(5)
-    locate_and_click(add_m2k)
+    locate_and_click(image_path, "add_m2k.png")
     time.sleep(2)
-    locate_and_click(enable_demo_img)
+    locate_and_click(image_path, "enable_demo.png")
     time.sleep(2)
 
     # Disable Demo Mode
-    locate_and_click(home_img)
+    locate_and_click(image_path, "home.png")
     time.sleep(5)
-    locate_and_click(disable_demo_img)
+    locate_and_click(image_path, "disable_demo.png")
     time.sleep(2)
 
     # Identify M2K
-    locate_and_click(home_img)
+    locate_and_click(image_path, "home.png")
     time.sleep(5)
-    locate_and_click(identify_img)
+    locate_and_click(image_path, "identify.png")
     time.sleep(15)
 
     # Calibrate M2k
-    locate_and_click(home_img)
+    locate_and_click(image_path, "home.png")
     time.sleep(5)
-    locate_and_click(cal_img)
+    locate_and_click(image_path, "calibrate.png")
     time.sleep(15)
 
     # Disconnect M2k
-    locate_and_click(home_img)
+    locate_and_click(image_path, "home.png")
     time.sleep(5)
-    locate_and_click(disconnect_img)
+    locate_and_click(image_path, "disconnect.png")
     time.sleep(3)
     print("Done!")
 
