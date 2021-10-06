ab1a4bb5460dd60cf53811a50baf1c11d95bc6d3
Hannah Rosete <Hannah.Rosete@analog.com>
2021-10-06 20:48:07 +0800

    Codes under test
    Signed-off-by: Hannah Rosete <Hannah.Rosete@analog.com>

--- test/sandbox.py
+++ test/sandbox.py
@@ -1,112 +1,48 @@
-from os.path import dirname, join, realpath
-
 import os
-import pyautogui as pag
+import pyautogui
+import pytesseract
+import cv2
 import time
+from PIL import Image
 
-# image_path = dirname(realpath(__file__)) + "/scopy_images/"
-# m2k_image = image_path + "m2k.png"
-connect_image = image_path + "connect.png"
-identify_image = image_path + "identify.png"
-cal_image = image_path + "calibrate.png"
-osc_image = image_path + "oscilloscope.png"
-spectrum_analyzer_image = image_path + "spectrum_analyzer.png"
-network_analyzer_image = image_path + "network_analyzer.png"
-sig_gen_image = image_path + "signal_generator.png"
-logic_analyzer_image = image_path + "logic_analyzer.png"
-pattern_generator_image = image_path + "pattern_generator.png"
-dio_image = image_path + "digital_io.png"
-voltmeter_image = image_path + "voltmeter.png"
-power_image = image_path + "power_supply.png"
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+image_path = os.path.join(os.getcwd(), "scopy_images")
 
+img = os.path.join(image_path, "m2k_usb.png")
+image = cv2.imread(img)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+x, thresh = cv2.threshold(gray, 90, 255, cv2.THRESH_BINARY)
 
-def locate_and_click(img):
-    """locate_and_click: Locate the specified image in the UI and click it.
+# Crop images
+crop_top = image[0:30, 0:150]
+crop_bottom = image[160:193, 0:150]
 
-    parameter:
-        img: type=string
-            Filename with extension of the image you want to find
-    """
-    image = pag.locateCenterOnScreen(img)
-    pag.click(image)
+# Threshold cropped images
+x, thresh_top = cv2.threshold(crop_top, 90, 255, cv2.THRESH_BINARY)
+y, thresh_bottom = cv2.threshold(crop_bottom, 90, 255, cv2.THRESH_BINARY)
 
+# Print shapes
+# print(type(image))  # <class 'numpy.ndarray'>
+# print(image.shape)
 
-def main():
-    dir = "C:\\Users\\hrosete\\Downloads\\Analog Devices\\Scopy\\Scopy.exe"
+ref = pytesseract.image_to_string(thresh)
+print(f"Texts in Threshed Image: {ref}")
 
-    # Open Scopy
-    try:
-        os.startfile(str(dir))
-    except Exception as e:
-        print(str(e))
-        assert False, "Kindly install the latest release of Scopy here: https://github.com/analogdevicesinc/scopy/releases"
+if "ip:192" in ref:
+    print("IP Connected")
+elif "ip:127" in ref:
+    print("Demo Mode")
+elif "usb" in ref:
+    print("Serially Connected")
 
-    # Wait for the program to open
-    time.sleep(3)
+# Show images
+# cv2.imshow("Image", image)
+# cv2.imshow("Grayscale", gray)
+# cv2.imshow("Cropped Top", crop_top)
+# cv2.imshow("Cropped Bottom", crop_bottom)
+# cv2.imshow("Thresh Cropped Top", thresh_top)
+# cv2.imshow("Thresh Cropped Bottom", thresh_bottom)
+cv2.imshow("Threshold", thresh)
 
-    # Connect to M2K
-    locate_and_click(m2k_image)
-    time.sleep(2)
-    locate_and_click(connect_image)
-    # while pyautogui.locateCenterOnScreen("calibrating.png"):
-    #     pyautogui.PAUSE
-    time.sleep(2)
-
-    calibrating = pyautogui.locateCenterOnScreen(
-        calibrating_image, confidence=0.7)
-    connecting = pyautogui.locateCenterOnScreen(
-        connecting_image, confidence=0.7)
-    if calibrating or connecting:
-        time.sleep(2)
-    else:
-        print("Connected!")
-
-    # Identify M2K
-    locate_and_click(identify_image)
-    time.sleep(3)
-
-    # Calibrate M2K
-    locate_and_click(cal_image)
-    time.sleep(3)
-
-    # Oscilloscope
-    locate_and_click(osc_image)
-    '''should call on the osc module after'''
-
-    # Spectrum Analyzer
-    locate_and_click(spectrum_analyzer_image)
-    '''should call on the spec module after'''
-
-    # # Network Analyzer
-    # locate_and_click(network_analyzer_image)
-    # '''should call on the network module after'''
-
-    # # Signal Generator
-    # locate_and_click(sig_gen_image)
-    # '''should call on the siggen module after'''
-
-    # # Logic Analyzer
-    # locate_and_click(logic_analyzer_image)
-    # '''should call on the logic module after'''
-
-    # # Pattern Generator
-    # locate_and_click(pattern_generator_image)
-    # '''should call on the pattern module after'''
-
-    # # Digital IO
-    # locate_and_click(dio_image)
-    # '''should call on the digital io module after'''
-
-    # # Voltmeter
-    # locate_and_click(voltmeter_image)
-    # '''should call on the voltmeter module after'''
-
-    # # Power Supply
-    # locate_and_click(power_image)
-    # '''should call on the power supply module after'''
-
-    # # About
-
-
-if __name__ == "__main__":
-    main()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
